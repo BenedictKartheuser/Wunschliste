9c1fc5b9b346b5628f0c944355b4d8678e3a27c4
Benedict Kartheuser <kartbene@gmx.de>
2019-11-04 12:08:35 +0100

    Added sidebar to start page

--- src/app.js
+++ src/app.js
@@ -29,4 +29,16 @@ class App {
             menu.classList.add("small-screen-hidden");
         }
     }
+
+        /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+    function openNav() {
+      document.getElementById("sidenav").style.width = "250px";
+      document.getElementById("app-main-area").style.marginLeft = "250px";
+    }
+
+    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+      document.getElementById("sidenav").style.width = "0";
+      document.getElementById("app-main-area").style.marginLeft = "0";
+    }
 }
